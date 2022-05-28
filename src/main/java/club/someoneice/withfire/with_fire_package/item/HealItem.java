7ac32c5c5b5e68afbd108432273fa042780400c2
Amarok <91077200+AmarokIce@users.noreply.github.com>
2022-05-28 16:07:27 +0800

    Fix Bandage.

--- src/main/java/club/someoneice/withfire/with_fire_package/item/HealItem.java
+++ src/main/java/club/someoneice/withfire/with_fire_package/item/HealItem.java
@@ -1,11 +1,13 @@
 package club.someoneice.withfire.with_fire_package.item;
 
+import club.someoneice.withfire.Withfire;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
@@ -13,16 +15,23 @@ import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 
 public class HealItem extends Item {
+    private static final FoodProperties food = ((new FoodProperties.Builder()).alwaysEat().build());
     int heal;
 
     public HealItem(int heal) {
-        super(new Item.Properties());
+        super(new Item.Properties().tab(Withfire.TAB).stacksTo(8).food(food));
         this.heal = heal;
     }
 
     @Override
     public UseAnim getUseAnimation(ItemStack item) {
-        return UseAnim.EAT;
+        return UseAnim.BOW;
+    }
+
+
+    @Override
+    public int getUseDuration(ItemStack itemStack) {
+        return 16;
     }
 
     @Override
@@ -35,7 +44,6 @@ public class HealItem extends Item {
         if (player instanceof Player) {
             world.playSound((Player) null, player.blockPosition(), SoundEvents.PLAYER_HURT, SoundSource.NEUTRAL, 1.0F, 1.0F + world.random.nextFloat() * 0.3F);
         }
-
         player.heal(this.heal);
         item.shrink(1);
         return item;
